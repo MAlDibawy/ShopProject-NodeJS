cb99a3297325d757bfc2ea12634c3355d07feddf
MAlDibawy <34455650+MAlDibawy@users.noreply.github.com>
2022-06-25 01:00:14 +0200

    modifying view by adding ejs template engine

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,23 @@ const path = require('path');
 const rootDirectory = require('../util/path');
 
 const router = express.Router();
+const products = [];
 
 router.get('/addProduct', (req, res, next) => {
-    res.sendFile(path.join(rootDirectory, 'views', 'addProduct.html'));
+    // res.sendFile(path.join(rootDirectory, 'views', 'addProduct.html'));
+    res.render('addProduct', {
+        pageTitle: 'Add Product',
+        path: '/admin/addProduct',
+    });
 });
 router.post('/addProduct', (req, res, next) => {
+    var productTitle = req.body.title;
+    products.push({
+        title: productTitle
+    });
     // console.log(JSON.parse(JSON.stringify(req.body)));
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,17 @@ const path = require('path');
 const rootDirectory = require('../util/path');
 
 const router = express.Router();
-
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
+    // console.log(adminData.products);
+    const shopProducts = adminData.products;
+    res.render('shop', {
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: shopProducts.length > 0,
+        products: shopProducts
+    });
     res.sendFile(path.join(rootDirectory, 'views', 'shop.html'));
 });
 
